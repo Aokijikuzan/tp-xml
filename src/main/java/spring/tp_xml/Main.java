7a4d125aee5e7115f86069dc8abb3ee1c716d4bb
florent <florent@gmail.com>
2018-10-17 12:15:52 +0200

    2e commit

--- src/main/java/spring/tp_xml/Main.java
+++ src/main/java/spring/tp_xml/Main.java
@@ -1,13 +1,21 @@
 package spring.tp_xml;
 
+import javax.naming.Context;
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext();
+	AbstractApplicationContext context = new ClassPathXmlApplicationContext("/xmlFile/applicationContext.xml");
 		System.out.println("context loaded!");
+		System.out.println("--------------------");
+		MyObject objA=(MyObject)context.getBean("myObject1");
+		MyObject obj=(MyObject)context.getBean("myObject2");
+		System.out.println("Object"+objA);
+		System.out.println("--------------------");
+		System.out.println(objA.getName() +"->"+obj.getParent().getName());
 	}
 
 }
